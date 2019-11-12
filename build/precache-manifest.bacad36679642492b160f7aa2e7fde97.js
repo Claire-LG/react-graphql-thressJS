e1cb8bf906b9961c4a4d5bc1fdc8f085c88451e1
claire <liangge@vpgame.cn>
2019-11-12 18:36:17 +0800

    add:first commit

---
+++ build/precache-manifest.bacad36679642492b160f7aa2e7fde97.js
@@ -0,0 +1,394 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06234d21b1bdb1cf5e4ccc427552a367",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6bfcfdafb562c7c2a50",
+    "url": "/static/css/main.f1d57b26.chunk.css"
+  },
+  {
+    "revision": "2faeaea337be391871a8",
+    "url": "/static/js/2.a8b633c1.chunk.js"
+  },
+  {
+    "revision": "c6bfcfdafb562c7c2a50",
+    "url": "/static/js/main.b05d2295.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "82a3c55e483207ed16fe635a4509f3bc",
+    "url": "/static/media/2风机非电机侧轴承.82a3c55e.glb"
+  },
+  {
+    "revision": "a82efd69b01be22fb1b5a98b1ed36a7e",
+    "url": "/static/media/4风机电机侧轴承.a82efd69.glb"
+  },
+  {
+    "revision": "3bfae78411237a45460c7fc8254658d2",
+    "url": "/static/media/5联轴器.3bfae784.glb"
+  },
+  {
+    "revision": "2bab9b509b5c1662fc6c9f1aff3cc1b3",
+    "url": "/static/media/6电机负荷侧轴承.2bab9b50.glb"
+  },
+  {
+    "revision": "43e2bb24d5d58eaa288f8987139f7b5b",
+    "url": "/static/media/7电机.43e2bb24.glb"
+  },
+  {
+    "revision": "70e97eccdc5e1d57c1e6b4ae2c124e42",
+    "url": "/static/media/8电机自由侧轴承.70e97ecc.glb"
+  },
+  {
+    "revision": "970eab715556bc3fdd2742efd7f71cf8",
+    "url": "/static/media/BigBack.970eab71.png"
+  },
+  {
+    "revision": "b0c36f84cc9e1c3a5116b26de434944c",
+    "url": "/static/media/alert.b0c36f84.png"
+  },
+  {
+    "revision": "acb76c2f12da80f9ded9eb8e976f471c",
+    "url": "/static/media/alert1.acb76c2f.png"
+  },
+  {
+    "revision": "e30faea8066fe849d211c54b13dc3a35",
+    "url": "/static/media/alertNormal.e30faea8.png"
+  },
+  {
+    "revision": "c2b46017c6f61aae41e5f96043e867e3",
+    "url": "/static/media/alertTipBj.c2b46017.png"
+  },
+  {
+    "revision": "3456387d23fe60f05c20a4883a2a816c",
+    "url": "/static/media/axial_flow.3456387d.png"
+  },
+  {
+    "revision": "57fa462987860660078f1ffd927469e3",
+    "url": "/static/media/axial_flow.57fa4629.png"
+  },
+  {
+    "revision": "30361f789486e0cfdc46bd79470b6281",
+    "url": "/static/media/bg.30361f78.png"
+  },
+  {
+    "revision": "6edb18824001f8a1fc15f7b559c2c33a",
+    "url": "/static/media/bg1.6edb1882.png"
+  },
+  {
+    "revision": "71875cb34394daca7629f7af7ac11963",
+    "url": "/static/media/bj.71875cb3.png"
+  },
+  {
+    "revision": "d47600fb795cd730d7616c3a5d7ca551",
+    "url": "/static/media/brokenLine.d47600fb.png"
+  },
+  {
+    "revision": "b1b31c9372cefe1ef4405891ebb5ff03",
+    "url": "/static/media/bubble.b1b31c93.png"
+  },
+  {
+    "revision": "0cc42456207ce71dafd7f9d44a2b7ffe",
+    "url": "/static/media/cantilever.0cc42456.png"
+  },
+  {
+    "revision": "9ad51f79dc24120587719ff021ed0b4d",
+    "url": "/static/media/cantilever.9ad51f79.png"
+  },
+  {
+    "revision": "ec736e41bd67b710e2e0a29420bd2c31",
+    "url": "/static/media/card.ec736e41.png"
+  },
+  {
+    "revision": "3159571056865542fb2ac3aa008032be",
+    "url": "/static/media/circle.31595710.png"
+  },
+  {
+    "revision": "d18e3cd5abfdfc5a2fd56846f839608b",
+    "url": "/static/media/double.d18e3cd5.png"
+  },
+  {
+    "revision": "48a0f23aff18f8eca5892249c60bf49a",
+    "url": "/static/media/double_suction.48a0f23a.png"
+  },
+  {
+    "revision": "b932ee84ba9400833275769699cc2277",
+    "url": "/static/media/durationNormal.b932ee84.png"
+  },
+  {
+    "revision": "465ad83c9e85c41fc0fb3a56a050242f",
+    "url": "/static/media/efficiency.465ad83c.png"
+  },
+  {
+    "revision": "2a83a78e0c8d5fb4a274671e7938def4",
+    "url": "/static/media/efficiencyFault.2a83a78e.png"
+  },
+  {
+    "revision": "9fd7dfd47e93a0f0975ead0d64ec7654",
+    "url": "/static/media/efficiencyNormal.9fd7dfd4.png"
+  },
+  {
+    "revision": "ed92bba50489ba02e1a3dd681fb29483",
+    "url": "/static/media/efficiencyWarning.ed92bba5.png"
+  },
+  {
+    "revision": "fa42dd644f30600281b946169dab2b41",
+    "url": "/static/media/evaluation.fa42dd64.png"
+  },
+  {
+    "revision": "4cc1b1fa04f2f6144a278007ee08933a",
+    "url": "/static/media/fan.4cc1b1fa.png"
+  },
+  {
+    "revision": "1e917b940b2e0d3abf6218f475be1cbc",
+    "url": "/static/media/fan1.1e917b94.png"
+  },
+  {
+    "revision": "0969d3c823b0114ec82dc4857476c662",
+    "url": "/static/media/fan2.0969d3c8.png"
+  },
+  {
+    "revision": "64208ab583c90d0fc4dd73063334174e",
+    "url": "/static/media/fan3.64208ab5.png"
+  },
+  {
+    "revision": "1f469736c546a4ef753309c9d3bac90e",
+    "url": "/static/media/fan4.1f469736.png"
+  },
+  {
+    "revision": "e4e2f1ef5540e3fa2e6abb85f9edcfd0",
+    "url": "/static/media/fault.e4e2f1ef.png"
+  },
+  {
+    "revision": "18e93cbfaba2ed6c0c4a87e56cef8c50",
+    "url": "/static/media/float.18e93cbf.png"
+  },
+  {
+    "revision": "1e96cc6010e9c9c02c0c5ea17fd415e2",
+    "url": "/static/media/float.1e96cc60.png"
+  },
+  {
+    "revision": "344e118fa2dc799b89a57b1a12100939",
+    "url": "/static/media/headBg.344e118f.png"
+  },
+  {
+    "revision": "5e2c326c64dc4121890ce668d2774b32",
+    "url": "/static/media/health.5e2c326c.png"
+  },
+  {
+    "revision": "fddeb5eba334a2e97f64b914bdd5156c",
+    "url": "/static/media/left1.fddeb5eb.png"
+  },
+  {
+    "revision": "624fe09b2a5505dec320f73543ea6cc7",
+    "url": "/static/media/left2.624fe09b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cfd07aa5b5132daf620de579289e94a4",
+    "url": "/static/media/logo.cfd07aa5.png"
+  },
+  {
+    "revision": "f8c60ce43c0a908f3dd2507b359d925b",
+    "url": "/static/media/noRecord.f8c60ce4.png"
+  },
+  {
+    "revision": "510c3b2accb611b9763af4926b907c73",
+    "url": "/static/media/right1.510c3b2a.png"
+  },
+  {
+    "revision": "7f5641745dd4a707dcb3457b40104494",
+    "url": "/static/media/right2.7f564174.png"
+  },
+  {
+    "revision": "85dc280378f3593d7ef345b6e99e916a",
+    "url": "/static/media/right3.85dc2803.png"
+  },
+  {
+    "revision": "fc07f0cac1b1108bc78555aa9c7dcaf4",
+    "url": "/static/media/right4.fc07f0ca.png"
+  },
+  {
+    "revision": "de0a826bae75a8e8bc6814b90c12907c",
+    "url": "/static/media/single_suction.de0a826b.png"
+  },
+  {
+    "revision": "f3293cbda5814348d107d98191e7cd41",
+    "url": "/static/media/single_suction.f3293cbd.png"
+  },
+  {
+    "revision": "25d0a92c16417283b5084699f776e1d7",
+    "url": "/static/media/textWrap.25d0a92c.png"
+  },
+  {
+    "revision": "d32755b3ebe578f0d0cd1ec9b79ae09a",
+    "url": "/static/media/threeMenu.d32755b3.png"
+  },
+  {
+    "revision": "1d062585cc1186a3e74f56b080fcad83",
+    "url": "/static/media/time.1d062585.png"
+  },
+  {
+    "revision": "e7bd4994d3bec8d0ab16084de448d8a3",
+    "url": "/static/media/titleBg.e7bd4994.png"
+  },
+  {
+    "revision": "ffc2d9a00ab18867c5281bfa9388e043",
+    "url": "/static/media/单吸1风机叶轮.ffc2d9a0.glb"
+  },
+  {
+    "revision": "f08dc74d076ffddad716d82e924f2410",
+    "url": "/static/media/单吸3风机外壳.f08dc74d.glb"
+  },
+  {
+    "revision": "0809722c72d99be060339a736116a626",
+    "url": "/static/media/单吸5联轴器.0809722c.glb"
+  },
+  {
+    "revision": "eac468cb7e3d0672e2cc436218183af3",
+    "url": "/static/media/单吸6电机.eac468cb.glb"
+  },
+  {
+    "revision": "ae527fd37e1a28d095ebfa74e7879ca0",
+    "url": "/static/media/单吸7电机负荷侧轴承.ae527fd3.glb"
+  },
+  {
+    "revision": "daa97aa3b68fc8aa4ceac748515e2cb8",
+    "url": "/static/media/单吸8电机自由侧轴承.daa97aa3.glb"
+  },
+  {
+    "revision": "e914c3ba1a1946c2a275107be4c497ff",
+    "url": "/static/media/叶轮1.e914c3ba.glb"
+  },
+  {
+    "revision": "f51951c6fb06fe1dbc29b45a48c96771",
+    "url": "/static/media/外壳.f51951c6.glb"
+  },
+  {
+    "revision": "4dbcf89155488231487664bb9c5641a2",
+    "url": "/static/media/外壳1.4dbcf891.glb"
+  },
+  {
+    "revision": "846f09f0789a67dbf6cf0ce816f4cc05",
+    "url": "/static/media/悬臂1风机叶轮.846f09f0.glb"
+  },
+  {
+    "revision": "358020bc58c8de9705513556cbe00f46",
+    "url": "/static/media/悬臂3风机非电机侧轴承.358020bc.glb"
+  },
+  {
+    "revision": "eb7854f224befec8a905cb35b9d689fe",
+    "url": "/static/media/悬臂4联轴器.eb7854f2.glb"
+  },
+  {
+    "revision": "d559eae3e0346a08acfed55d723f93c7",
+    "url": "/static/media/悬臂5电机负荷侧轴承.d559eae3.glb"
+  },
+  {
+    "revision": "1a309a9544c5e2296d669543b8461756",
+    "url": "/static/media/悬臂6电机.1a309a95.glb"
+  },
+  {
+    "revision": "b6ad0c4755707cf4f71bf99843ec2d0c",
+    "url": "/static/media/悬臂7电机自由侧轴承.b6ad0c47.glb"
+  },
+  {
+    "revision": "44cc423def0df18b0fef397999bfe0d5",
+    "url": "/static/media/电机.44cc423d.stl"
+  },
+  {
+    "revision": "78892e6231f61362079fb479debc4181",
+    "url": "/static/media/电机.78892e62.glb"
+  },
+  {
+    "revision": "f31800cbf3f92d86ddcf6ec49c2f8641",
+    "url": "/static/media/电机侧.f31800cb.glb"
+  },
+  {
+    "revision": "1b097ec9d9f25406026cc1e4b127f399",
+    "url": "/static/media/电机自由侧轴承.1b097ec9.glb"
+  },
+  {
+    "revision": "c8056c34199e4f12fe839c5523d294de",
+    "url": "/static/media/电机自由侧轴承.c8056c34.stl"
+  },
+  {
+    "revision": "4e6d9a441d703051a867cec7b8f91b5f",
+    "url": "/static/media/电机负荷侧轴承.4e6d9a44.stl"
+  },
+  {
+    "revision": "d841380691a590bd6c6efbeac89e522c",
+    "url": "/static/media/电机负荷侧轴承.d8413806.glb"
+  },
+  {
+    "revision": "f2e779c130890888997745b06cc2d29d",
+    "url": "/static/media/联轴器.f2e779c1.stl"
+  },
+  {
+    "revision": "fe966c861b8d942d9603d87b5b780ed0",
+    "url": "/static/media/联轴器.fe966c86.glb"
+  },
+  {
+    "revision": "5988788880c885c09219427e91e926d2",
+    "url": "/static/media/轴承.59887888.stl"
+  },
+  {
+    "revision": "99cbfe98e4c506fae5c59a42ce23659f",
+    "url": "/static/media/轴承.99cbfe98.glb"
+  },
+  {
+    "revision": "66c5be5f687862dec98f0b409c802541",
+    "url": "/static/media/轴承底座.66c5be5f.glb"
+  },
+  {
+    "revision": "1bb755ddc18f0daf2e608b8c1a1ed8b6",
+    "url": "/static/media/静调1风机外壳.1bb755dd.stl"
+  },
+  {
+    "revision": "d6d765f5ff66e917d54cae2f7fb17655",
+    "url": "/static/media/静调1风机外壳.d6d765f5.glb"
+  },
+  {
+    "revision": "af7d47badffa792598a5f1fe69c4f4c0",
+    "url": "/static/media/非电机侧.af7d47ba.glb"
+  },
+  {
+    "revision": "32a44eb06f384afc906d1eff42b7be81",
+    "url": "/static/media/风机叶轮.32a44eb0.glb"
+  },
+  {
+    "revision": "79edffa34fbe072c887dcc858563cf09",
+    "url": "/static/media/风机叶轮.79edffa3.stl"
+  },
+  {
+    "revision": "3a416ce1afb6254601402bf3f4db5573",
+    "url": "/static/media/风机电机侧轴承.3a416ce1.stl"
+  },
+  {
+    "revision": "54ebad24f0561f2c2297e4901ceb87ef",
+    "url": "/static/media/风机电机侧轴承.54ebad24.glb"
+  },
+  {
+    "revision": "ffbd100417c590cb448a6b7f54945204",
+    "url": "/static/media/风机电机侧轴承.ffbd1004.glb"
+  },
+  {
+    "revision": "f4379d0857f59fcdb497446b238c3739",
+    "url": "/static/media/风机电机侧轴承1.f4379d08.glb"
+  },
+  {
+    "revision": "4ce9e136d61ffe7fa64609185ed46ee4",
+    "url": "/static/media/风机非电机侧轴承.4ce9e136.glb"
+  },
+  {
+    "revision": "7a23bd0eae9c739f66a0316753c3d0e8",
+    "url": "/static/media/风机非电机侧轴承.7a23bd0e.stl"
+  }
+]);
